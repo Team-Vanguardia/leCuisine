aabe12571262156e120625913f0f304b5db3d2f1
Braulio Calix <djbrzhn1@hotmail.com>
2022-08-30 16:30:54 -0600

    update login
    Cambios que pidieron para el login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,7 @@
 import tw from "twrnc";
 import { View, Text, StatusBar, Pressable, Button, ImageBackground, TextInput } from "react-native";
 import fondo from "../Imagenes/fondo1.jpg";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/core";
 import { auth, signin, signup } from "../../firebase";
 
@@ -12,44 +12,46 @@ export default function LogIn() {
   const navigation = useNavigation()
 
   useEffect(() => {
-   const unsubscribe = auth.onAuthStateChanged (user => {
-     if (user) {
-       navigation.navigate("Home")
-     }
-   })
-   return unsubscribe
- }, [])
- 
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigation.navigate("Home")
+      }
+    })
+    return unsubscribe
+  }, [])
+
   function handleSingUp() {
-    signup(email,password)
+    signup(email, password)
 
   }
-  
+
   const handleLogin = () => {
-    signin(email,password)
+    signin(email, password)
   }
- 
+
   return (
     <View style={tw`justify-center items-center`}>
       <ImageBackground source={fondo} style={tw`w-100 h-190 `}>
         <View
           style={tw` self-auto items-center -p-5  justify-center mt-60 ml-25 w-1/2 rounded-3xl bg-gray-900  h-3/10`}
         >
-          <Text style={tw`text-white text-xl  py-5 `}>𝓛𝓸𝓰𝓲𝓷</Text>
+          <Text style={tw`text-white text-xl   `}>𝓛𝓸𝓰𝓲𝓷</Text>
           <Text style={tw`text-white text-xl  justify-start `}>𝓤𝓼𝓮𝓻𝓷𝓪𝓶𝓮</Text>
           <TextInput style={tw` bg-white rounded-lg w-8/10 h-1/9 text-center`} placeholder=" 𝓤𝓼𝓮𝓻𝓷𝓪𝓶𝓮" value={email} onChangeText={text => setEmail(text)} />
           <View style={tw`mt-0.9`}></View>
           <Text style={tw`text-white text-xl  justify-start `}>𝓟𝓪𝓼𝓼𝔀𝓸𝓻𝓭</Text>
           <TextInput style={tw` bg-white rounded-lg w-8/10 h-1/9 text-center`} placeholder=" 𝓟𝓪𝓼𝓼𝔀𝓸𝓻𝓭" value={password} onChangeText={text => setPassword(text)} secureTextEntry={true} />
-          <View style={tw`mt-5 justify-center items-center mb-5 `}>
+          <View style={tw`mt-5 ml-20  justify-center items-center  `}>
             <Button style={tw` bg-blue-100 text-red-600 `} onPress={handleLogin} title="𝓢𝓲𝓰𝓷 𝓲𝓷 " />
-            <Button style={tw` bg-blue-100 text-red-600 `} onPress={handleSingUp} title="𝓢𝓲𝓰𝓷 up" />
           </View>
+          <View style={tw`-mt-9 mr-20  justify-center items-center  `}>
+          <Button style={tw` bg-blue-100 text-red-600 `} onPress={handleSingUp} title="𝓢𝓲𝓰𝓷 𝓾𝓹 " />
+        </View>
         </View>
         <View style={tw`items-center`}>
           <Text style={tw`mt-55  text-gray-300 text-4xl `}> 𝓛𝓮 𝓒𝓸𝓾𝓲𝓼𝓲𝓷𝓮</Text>
         </View>
-      </ImageBackground>
-    </View>
+      </ImageBackground >
+    </View >
   );
 }
